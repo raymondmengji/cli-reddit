3bfc9dfd18a9b388c63314242848c6f688e0c3d5
Raymond Ji <raymondji@berkeley.edu>
2021-05-20 15:38:11 -0700

    update

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import requests
-from prompt_toolkit import prompt, PromptSession
+import html
+from prompt_toolkit import prompt, PromptSession, print_formatted_text
 from prompt_toolkit.formatted_text import HTML
 from auth import SECRET_ID, SECRET_KEY
 import commands
@@ -19,26 +20,62 @@ token = 'bearer ' + d['access_token']
 API_URL = 'https://oauth.reddit.com'
 HEADER = {'Authorization': token, 'User-agent': 'your bot 0.1'}
 
+def main_toolbar():
+    return HTML('CLI for <b><style bg="ansired">Reddit</style></b>')
+
 def search_toolbar():
     return HTML('Subreddit <b><style bg="ansired">Search</style></b>')
 
-def main_toolbar():
-    return HTML('CLI for <b><style bg="ansired">Reddit</style></b>')
+def subreddit_search_toolbar():
+    return HTML('Subreddit <b><style bg="ansired">Search</style></b>')
 
 if __name__ == "__main__":
 	session = PromptSession()
 	while True:
+		# pre-process command
 		command = session.prompt("> ", bottom_toolbar=main_toolbar)
-		# print("command entered:", command)
-		if command == 's' or command == 'search':
-			text = session.prompt("Subreddit search: ", bottom_toolbar=search_toolbar)
-			param = {'q': text, 'limit': 5, 'sort': 'relevance'}
-			# print(param)
-			commands.search(param)
-		elif command == 'h' or command == 'help':
-			print('Type \'search\' or \'s\' to search for a subreddit.')
-			print('Type \'quit\' or \'q\' to quit the CLI.')
-		elif command == 'q' or command == 'quit':
+		command = command.strip()
+		if len(command) == 0:
+			continue
+		command_arr = command.split(" ")
+		# search for subreddit
+		if command_arr[0] == 's' or command_arr[0] == 'search':
+			if len(command_arr) == 1:
+				text = session.prompt("Subreddit search: ", bottom_toolbar=search_toolbar)
+				param = {'q': text, 'limit': 15, 'sort': 'relevance'}
+			else:
+				param = {'q': ' '.join(command_arr[1:]), 'limit': 15, 'sort': 'relevance'}
+			values = commands.search(param)
+			data = values['data']['children']
+			for i in range(len(data)):
+				print(data[i]['data']['display_name'])
+		# search within specified subreddit
+		elif command_arr[0] == 'ss' or command_arr[0] == 'subreddit-search':
+			if len(command_arr) == 1:
+				subreddit = session.prompt("Search in subreddit: ", bottom_toolbar=subreddit_search_toolbar)
+			else:
+				subreddit = ' '.join(command_arr[1:])
+			mode = session.prompt("Select mode (hot, new, top): ", bottom_toolbar=subreddit_search_toolbar)
+			if mode != 'hot' and mode != 'new' and mode != 'top':
+				print_formatted_text(HTML('<style bg="ansired">Default to hot</style>'))
+				mode = 'hot'
+			param = {'limit': 15}
+			if mode == 'top':
+				param['t'] = 'week'
+			values = commands.subreddit_search(subreddit, mode, param)
+			data = values['data']['children']
+			for i in range(len(data)):
+				print_formatted_text(HTML('<b><u><ansired>' + html.unescape(data[i]['data']['title']) + '</ansired></u></b>'))
+				print(data[i]['data']['url'])
+				print(html.unescape(data[i]['data']['selftext'].strip()))
+		# help command
+		elif command_arr[0] == 'h' or command_arr[0] == 'help':
+			if len(command_arr) == 1:
+				print('Type \'search\' or \'s\' to search for a subreddit.')
+				print('Type \'subreddit-search\' or \'ss\' to search within a subreddit.')
+				print('Type \'quit\' or \'q\' to quit the CLI.')
+		# quit command
+		elif command_arr[0] == 'q' or command_arr[0] == 'quit':
 			break
 		else:
 			print("Invalid command.")
--- commands.py
+++ commands.py
@@ -1,12 +1,16 @@
 import requests
-
 import app
 
-
+# api request for relevant subreddits
 def search(param):
     response = requests.get(app.API_URL + '/subreddits/search', headers=app.HEADER, params=param)
-    # print('Status Code: ', response.status_code)
-    values = response.json()
-
-    for i in range(len(values['data']['children'])):
-        print(values['data']['children'][i]['data']['display_name'])
+    return response.json()
+# api request for posts in specified subreddit
+def subreddit_search(subreddit, mode, param):
+    url = subreddit + "/" + mode
+    if 't' in param:
+        url += "/?t=" + param['t']
+    print("Searching: reddit.com/r/" + url + "...")
+    response = requests.get(app.API_URL + '/r/' + subreddit + '/' + mode, headers=app.HEADER, params = param)
+    return response.json()
+    
